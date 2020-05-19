5921fca550ecdafda9cdf271c9c19c729812249c
MiguelGomezC <60814215+MiguelGomezC@users.noreply.github.com>
2020-05-19 19:46:41 +0200

    Delete TextTimeVectorizer.py

--- Scripts/TextTimeVectorizer.py
+++
@@ -1,8 +0,0 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from scipy.sparse import hstack
-
-class TextTimeVectorizer():
-    def fit_transform(self,X):
-        tvec = TfidfVectorizer(ngram_range=(1,3), max_features=100000)
-        text_features = tvec.fit_transform(X['text'])
-        return hstack((text_features, X.time_class))
