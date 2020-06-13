f5a58707624af99b5f14f3202dbc4c863d812257
MiguelGomezC <60814215+MiguelGomezC@users.noreply.github.com>
2020-06-13 20:02:07 +0200

    Final

--- GridSearch.py
+++ GridSearch.py
@@ -2,12 +2,12 @@ import pandas as pd
 import numpy as np
 import seaborn as sn
 import matplotlib.pyplot as plt
-"""
-from imblearn.pipeline import Pipeline
-from imblearn.over_sampling import SMOTE, RandomOverSampler
-ros = RandomOverSampler(random_state=777)
+
+from imblearn.pipeline import Pipeline as imbpipe
+from imblearn.over_sampling import SMOTE
 smote = SMOTE(random_state=777)
-"""
+
+#Carga de datos
 import json
 with open('clean_data','r') as fichero:
     data = json.load(fichero)
@@ -17,43 +17,79 @@ Y = df['sentiment']
 
 from sklearn.pipeline import Pipeline
 
+#Vectorización de la característica tiempo via MinMaxScaler y texto via TFIDF
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.compose import ColumnTransformer
 ct = ColumnTransformer([("Scaler", MinMaxScaler(), ['time_feature']),\
                         ("Tfidfvectorizer", TfidfVectorizer(ngram_range=(1,3), max_features=50000),'text')])
-transformerparams = {'coltrans':[ct],'coltrans__TfidfVectorizer__ngram_range':[(1,1),(1,2),(1,3)],\
-               'coltrans__TfidfVectorizer__max_features':[50000]}
+
+#Métodos que vamos a utilizar
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import GridSearchCV
-
 from sklearn.ensemble import GradientBoostingClassifier
-#probar gradient boost, adaptative boost
+from sklearn.ensemble import AdaBoostClassifier
+
+from sklearn.tree import DecisionTreeClassifier
+DTC = DecisionTreeClassifier(max_features = "auto", max_depth = None)
 
 def pipe(obj):
     obj = classifiers[obj]['classifier'][0]
     return Pipeline([('coltrans', ct), ('classifier', obj)])
 
-classifiers = {'LogisticRegression': {'classifier':[LogisticRegression()], 'classifier__penalty' : ['l1', 'l2'], 'classifier_C' : np.logspace(-4, 4, 20)},\
+def resampling_pipe(obj):
+    obj = classifiers[obj]['classifier'][0]
+    return imbpipe([('coltrans', ct),('SMOTE', SMOTE()), ('classifier', obj)])
+
+#Diccionario con los parámetros a probar en la búsqueda por rejilla
+classifiers = {'LogisticRegression': {'classifier':[LogisticRegression()], 'classifier__penalty' : ['l1', 'l2'], 'classifier__C' : np.logspace(-4, 4, 20)},\
                'MultinomialNB': {'classifier': [MultinomialNB()], 'classifier__alpha': [0.01, 0.1, 1.0]},\
-               'KNeighborsClassifier': {'classifier': [KNeighborsClassifier()], 'classifier__n_neighbors': [1,3,5,7,10,13,15,20,25,30], 'classifier__weights': ['uniform', 'distance']},\
+               'KNeighborsClassifier': {'classifier': [KNeighborsClassifier()], 'classifier__n_neighbors': range(1,31), 'classifier__weights': ['uniform', 'distance']},\
                'SVC': {'classifier': [SVC()],'classifier__C':[1,10,100,1000],'classifier__gamma':[1,0.1,0.001,0.0001], 'classifier__kernel':['linear','rbf']},\
-               'RandomForestClassifier':{'classifier':[RandomForestClassifier()],'classifier__bootstrap': [True],'classifier__max_depth': [80, 90, 100, 110], 'classifier__max_features': [2, 3],'classifier__min_samples_leaf': [3, 4, 5],'classifier__min_samples_split': [8, 10, 12],'classifier__n_estimators': [100, 200, 300, 1000]}}
+               'RandomForestClassifier':{'classifier':[RandomForestClassifier()],'classifier__bootstrap': [True],'classifier__max_depth': range(5,32,2), 'classifier__max_features': [2, 3],'classifier__min_samples_leaf': [3, 4, 5],'classifier__min_samples_split': [8, 10, 12],'classifier__n_estimators': [100, 200, 300, 1000]},\
+               'GradientBoostingClassifier':{'classifier':[GradientBoostingClassifier()],'classifier__n_estimators':range(20,81,10),'classifier__max_depth':range(5,16,2), 'classifier__min_samples_split':range(200,1001,200)},\
+               'AdaBoostClassifier':{'classifier':[AdaBoostClassifier()], 'classifier__base_estimator':[DTC], 'classifier__base_estimator__max_depth':range(5,32,2), 'classifier__n_estimators':range(20,81,10)}}
+    
 #SVC: recall: C=10, gamma = 1,  kernel='linear'; accuracy: C=100, gamma=1, kernel = 'linear'
 #MultinomialNB: accuracy: alpha = 1; recall: alpha = 0.01
 def grid_search(classifier):
     """
-    (Returns best estimator)
     --NO RESAMPLING--
     """
     param_grid = (classifiers[classifier])
     pipeline = pipe(classifier)
-    evaluation = GridSearchCV(pipeline, param_grid, scoring = 'precision', refit=True, n_jobs=-1, verbose = 2)
+    evaluation = GridSearchCV(pipeline, param_grid, scoring = 'recall', refit=True, n_jobs=-1, verbose = 2)
     evaluation.fit(X,Y)
-    return evaluation.best_estimator_
+    print("La mejor combinación ha sido: ", evaluation.best_params_, "\nCon una puntuación de: ", evaluation.best_score_)
+    return (evaluation.best_params_, evaluation.best_score_)
 
-from main import classification
+def imb_grid_search(classifier):
+    """
+    --WITH RESAMPLING--
+    """
+    param_grid = (classifiers[classifier])
+    pipeline = resampling_pipe(classifier)
+    evaluation = GridSearchCV(pipeline, param_grid, scoring = 'recall', refit=True, n_jobs=-1, verbose = 2)
+    evaluation.fit(X,Y)
+    print("La mejor combinación ha sido: ", evaluation.best_params_, "\nCon una puntuación de: ", evaluation.best_score_)
+    return (evaluation.best_params_, evaluation.best_score_)
+
+if __name__ == "__main__":
+    import joblib
+    """
+    params = []
+    for key in classifiers.keys():
+        GS = grid_search(key)
+        params.append(GS) #Pares parámetros, puntuación
+    joblib.dump(params, 'Resultados Gridsearch\params.pkl')
+        
+    params = []
+    for key in classifiers.keys():
+        GS = imb_grid_search(key)
+        params.append(GS)
+    joblib.dump(params, 'Resultados Gridsearch\imb_params.pkl')
+    """
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pandas.plotting import table
 import numpy as np
 import seaborn as sn
 import matplotlib.pyplot as plt
@@ -12,10 +13,11 @@ Y = df['sentiment']
 
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import classification_report
 
-def classification(pipeline, n_splits=5, X=X, Y=Y, average_method = 'macro'):
+def classification(pipeline, title, n_splits=5, X=X, Y=Y, average_method = 'macro'):
     """
-    X: 2 column dataframe containing the text and time features
+    X: 2 column dataframe containing text and time features
     Y: dataframe column containing target sentiment values
     """
     kfold = StratifiedKFold(n_splits=n_splits, shuffle = True, random_state = 777)
@@ -46,27 +48,55 @@ def classification(pipeline, n_splits=5, X=X, Y=Y, average_method = 'macro'):
     frame = pd.DataFrame({'y_Actual':y_values,'y_Predicted':predictions}, columns = ['y_Actual','y_Predicted'])
     confusion_matrix = pd.crosstab(frame['y_Actual'], frame['y_Predicted'], rownames=['Actual'], colnames=['Predicted'], normalize=True)
     sn.heatmap(confusion_matrix, annot=True)
+    plt.title(title)
+    fig = plt.gcf()
+    images_dir = 'Imágenes para la memoria'
+    fig.savefig(f"{images_dir}/{title}.png")
     plt.show()
+    
+    report = classification_report(y_values,predictions,output_dict=True)
+    dataframe = pd.DataFrame(report).transpose()
+    results_dir ='Reportes de clasificación'
+    with open(f"{results_dir}/{title}.tex", 'a') as fichero:
+        fichero.write(title+'\n')
+        fichero.write(df.to_latex())
     
     print("accuracy: %.2f%% (+/- %.2f%%)" % (np.mean(accuracy), np.std(accuracy)))
     print("precision: %.2f%% (+/- %.2f%%)" % (np.mean(precision), np.std(precision)))
     print("recall: %.2f%% (+/- %.2f%%)" % (np.mean(recall), np.std(recall)))
     print("f1 score: %.2f%% (+/- %.2f%%)" % (np.mean(f1), np.std(f1)))
 
-#Preprocesado inicial de los datos: escalar la característica tiempo y procesar el texto con TF-IDF
+#Vectorización de la característica tiempo via MinMaxScaler y texto via TFIDF
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.compose import ColumnTransformer
 ct = ColumnTransformer([("Scaler",MinMaxScaler(), ['time_feature']),\
                         ("Tfidfvectorizer", TfidfVectorizer(ngram_range=(1,3), max_features=100000),'text')])
 
+#Función para crear una pipeline a partir de un método (obj)
 from imblearn.pipeline import make_pipeline
-from imblearn.over_sampling import SMOTE, RandomOverSampler
+from imblearn.over_sampling import SMOTE
 
-def ROS_pipeline(obj):
-    return make_pipeline(ct, RandomOverSampler(random_state=777),obj)
 def SMOTE_pipeline(obj):
     return make_pipeline(ct, SMOTE(random_state=777), obj)
 
+#Importar ahora los métodos que vamos a utilizar
 from sklearn.linear_model import LogisticRegression
-lr = LogisticRegression()
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import AdaBoostClassifier
+
+if __name__ == "__main__":
+    #Cargar la lista de métodos con los parámetros ajustados a partir de GridSearch.py
+    import joblib
+    methods = joblib.load('Resultados Gridsearch\imb_params.pkl')
+    for method, score in methods:
+        title = str(method['classifier']).split('(')[0]
+        input('Presione enter para aplicar el siguiente método')
+        print('Se está evaluando el método', title,'\nObtuvo una puntuación de ',score,' en la búsqueda de rejilla.\n')
+        classification(SMOTE_pipeline(method['classifier']) ,title)
+        print('\n')
